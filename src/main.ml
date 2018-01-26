afcbe8d807e35184e2ff450519b9faccb0b7e119
Edwin Török <edvin.torok@citrix.com>
2018-01-26 00:45:55 +0000

    wip

--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,63 @@ open Bos_setup
 open Testarossa
 open Lwt.Infix
 
-let ip = OS.Arg.conv ~docv:"IP" (fun s ->
+let ctx = Lwt.new_key ()
+
+(* grey *)
+let pp_time = Fmt.(styled `Black string |> styled `Bold)
+
+let msg_of_level = function
+  | Logs.App -> Logs_fmt.app_style, ""
+  | Debug -> Logs_fmt.debug_style, "debug"
+  | Warning -> Logs_fmt.warn_style, "warn"
+  | Info -> Logs_fmt.info_style, "info"
+  | Error -> Logs_fmt.err_style, "error"
+
+let pp_level ppf level =
+  let style, msg = msg_of_level level in
+  Fmt.pf ppf "%5a" Fmt.(styled style string) msg
+
+let pp_header ppf (level, header) =
+  let now = Debug.gettimestring () in
+  let name = "" in
+  let module_ = "" in
+  let id, task =
+    match Lwt.get ctx with
+    | Some (id, task) -> id, task
+    | None -> 0, ""
+  in
+  Fmt.pf ppf "[%a%a|%a|%d %s|%s|%s] " pp_time now pp_level level
+    Fmt.(option string |> styled Logs_fmt.app_style) header
+    id name
+    task module_
+
+let setup style_renderer level =
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level level;
+  Logs.set_reporter (Logs_fmt.reporter ~pp_header ());
+  Logs.debug (fun m -> m ~header:"x" "Initialized")
+
+open Cmdliner
+
+let common =
+  let docs = Manpage.s_common_options in
+  Term.(const setup $ Fmt_cli.style_renderer ~docs () $ Logs_cli.level ~docs ())
+
+let init () =
+  Logs.info (fun m -> m "started")
+
+let () =
+  let sdocs = Manpage.s_common_options in
+  let exits = Term.default_exits in
+  let default_cmd =
+    let doc = "a small system-level test framework using Xen-on-Xen" in
+    Term.(ret (const (fun _ -> `Help (`Pager, None)) $ common)),
+    Term.info "testarossa" ~version:"v1.2" ~doc ~sdocs ~exits
+  in
+  Term.exit @@ Term.(eval_choice ~catch:true default_cmd [
+      Cmd_init.cmd ~sdocs ~exits ~common
+    ])
+(*let ip = OS.Arg.conv ~docv:"IP" (fun s ->
     R.trap_exn Ipaddr.V4.of_string_exn s |>
     R.error_exn_trap_to_msg) Ipaddr.V4.pp_hum
 
@@ -106,3 +162,4 @@ let () =
   |> Logs.on_error_msg ~use:(fun () -> 80)
   |> Format.set_margin;
   Lwt_main.run (main ())
+*)
